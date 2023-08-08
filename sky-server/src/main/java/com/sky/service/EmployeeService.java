c6b92178b4859840ddd6ed702ee34bf95d438b04
linyubing <480771559@qq.com>
2023-08-08 16:55:24 +0800

    苍穹外卖添加员工

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,9 +1,16 @@
 package com.sky.service;
 
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
+import org.springframework.beans.BeanUtils;
 
 public interface EmployeeService {
+    /**
+     * 添加员工
+     * @param employeeDTO
+     */
+     void save(EmployeeDTO employeeDTO);
 
     /**
      * 员工登录
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,15 +12,44 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
     private EmployeeMapper employeeMapper;
+
+    /**
+     * 添加员工
+     * @param employeeDTO
+     */
+    @Override
+    public  void save(EmployeeDTO employeeDTO) {
+        //创建一个Employee对象
+        Employee employee=new Employee();
+        //对象属性拷贝
+        BeanUtils.copyProperties(employeeDTO,employee);
+        //设置账号的状态  默认为启用
+        employee.setStatus(StatusConstant.ENABLE);
+        //设置密码  默认为加密后的123456
+        employee.setPassword(DigestUtils.md5DigestAsHex((PasswordConstant.DEFAULT_PASSWORD).getBytes()));
+        //设置创建时间
+        employee.setCreateTime(LocalDateTime.now());
+        //设置修改时间
+        employee.setUpdateTime(LocalDateTime.now());
+        //设置创建人
+        employee.setCreateUser(BaseContext.getCurrentId());
+        //设置修改时间
+        employee.setUpdateUser(BaseContext.getCurrentId());
+        //调用mapper层
+        employeeMapper.insert(employee);
+    }
 
     /**
      * 员工登录
@@ -40,6 +72,8 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //密码比对
         // TODO 后期需要进行md5加密，然后再进行比对
+        //将前端传过来的密码进行md5加密后与数据库查询出来的密码进行比对
+        password= DigestUtils.md5DigestAsHex(password.getBytes());
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
